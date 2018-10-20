f5fb1fb7249999a7798e2b94fc77a56a8104b0b5
grednoud <>
2018-10-20 21:26:51 +0300

    Vector graphics and movement of objects.

---
+++ core/src/ru/codesteps/BattleSpaceGame.java
@@ -0,0 +1,26 @@
+package ru.codesteps;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import ru.codesteps.screens.GameScreen;
+
+public class BattleSpaceGame extends Game {
+	public SpriteBatch batch;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		this.setScreen(new GameScreen(this));
+	}
+
+	@Override
+	public void render() {
+		super.render();
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
+	}
+}
