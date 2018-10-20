68827f8483429a06a9327f210ef1dc2beb87c632
grednoud <grednoud@gmail.com>
2018-10-20 18:23:55 +0300

    Moving objects and events

--- core/src/ru/codesteps/BattleSpaceGame.java
+++ core/src/ru/codesteps/BattleSpaceGame.java
@@ -1,33 +1,26 @@
 package ru.codesteps;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class BattleSpaceGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture background;
-	
+import ru.codesteps.screens.GameScreen;
+
+public class BattleSpaceGame extends Game {
+	public SpriteBatch batch;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		background = new Texture("background.jpg");
+		this.setScreen(new GameScreen(this));
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(background, 0, 0);
-		batch.end();
+	public void render() {
+		super.render();
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
-		background.dispose();
 	}
 }
---
+++ core/src/ru/codesteps/screens/MenuScreen.java
@@ -0,0 +1,6 @@
+package ru.codesteps.screens;
+
+import ru.codesteps.screens.base.BaseScreen;
+
+public class MenuScreen extends BaseScreen {
+}
